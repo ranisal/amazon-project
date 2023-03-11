be745aa1e19d5602cecc4187de8b6c0e45285a0b
rani rani <rani.salsoltuion@gmail.com>
2023-03-10 18:22:15 -0600

    first commit

--- src/test/java/upskill/amazon/stepDef/AmazonHomePageSteps.java
+++ src/test/java/upskill/amazon/stepDef/AmazonHomePageSteps.java
@@ -3,8 +3,11 @@ package upskill.amazon.stepDef;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import upskill.amazon.pageAction.AmazonHomePageActions;
 
 public class AmazonHomePageSteps {
+	AmazonHomePageActions AmazonHomePageActionsobj = new AmazonHomePageActions();
+
 	@Given("^Open Amazon Homepage$")
 	public void open_Amazon_Homepage() throws Throwable {
 	
@@ -12,26 +15,32 @@ public class AmazonHomePageSteps {
 	}
 	@When("^Search for Mobile Phone cases$")
 	public void search_for_Mobile_Phone_cases() throws Throwable {
+		AmazonHomePageActionsobj.Searchmobilephonecases();
 	    
 	}
 	@When("^Search for screen protector$")
 	public void search_for_screen_protector() throws Throwable {
+		AmazonHomePageActionsobj.searchscreenprotector();
 	    
 	}
 	@When("^Search for camera protector$")
 	public void search_for_camera_protector() throws Throwable {
+		AmazonHomePageActionsobj.searchcameraprotector();
 	    
 	}
 	@Given("^Search for \"([^\"]*)\"$")
-	public void search_for(String a) throws Throwable {
+	public void search_for(String items) throws Throwable {
+		AmazonHomePageActionsobj.searchItems(items);;
 	   
 	}
 	@When("^Search for Purina One Natural$")
 	public void search_for_Purina_One_Natural() throws Throwable {
+		AmazonHomePageActionsobj.searchCatfood();
 	    
 	}
 	@When("^Search for Cat Toys$")
 	public void search_for_Cat_Toys() throws Throwable {
+		AmazonHomePageActionsobj.searchCatToys();
 	    
 	}
 	
--- src/test/java/upskill/amazon/stepDef/AmazonSearchResultSteps.java
+++ src/test/java/upskill/amazon/stepDef/AmazonSearchResultSteps.java
@@ -3,23 +3,31 @@ package upskill.amazon.stepDef;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import upskill.amazon.pageAction.AmazonSearchResultActions;
 
 public class AmazonSearchResultSteps {
+	AmazonSearchResultActions AmazonSearchResultActionsobj = new AmazonSearchResultActions();
+	
+	
 	@Then("^Item list should have only mobile phone cases related products$")
 	public void item_list_should_have_only_mobile_phone_cases_related_products() throws Throwable {
+		AmazonSearchResultActionsobj.verifymobilephonecasesItems();
 	    
 	}
 	@Then("^Item list should have only  screen protector related products$")
 	public void item_list_should_have_only_screen_protector_related_products() throws Throwable {
+		AmazonSearchResultActionsobj.verifyscreenprotectorItems();
 	    
 	}
 	@Then("^Item list should have only camera protector related products$")
 	public void item_list_should_have_only_camera_protector_related_products() throws Throwable {
+		AmazonSearchResultActionsobj.verifyCameraprotectorItems();
 	    
 	}
 	
 	@When("^Filter by \"([^\"]*)\"$")
 	public void filter_by(String items) throws Throwable {
+		AmazonSearchResultActionsobj.verifyBrandItems(items);
 	   
 	}
 
@@ -29,11 +37,13 @@ public class AmazonSearchResultSteps {
 	}
 	@Then("^Item list should have only Purina One Natural$")
 	public void item_list_should_have_only_Purina_One_Natural() throws Throwable {
+		AmazonSearchResultActionsobj.verifyCatfood();
 	   
 	}
 	
 	@When("^Click On Kelutd Cat Toys$")
 	public void click_On_Kelutd_Cat_Toys() throws Throwable {
+		AmazonSearchResultActionsobj.verifyCatToys();
 	  
 	}
 
